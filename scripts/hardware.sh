a5571328b2d1205944fd301fe5d1b181d5bb5846
jm2000 <jm8371@gmail.com>
2017-07-05 16:49:00 +0800

    Add hardware script and processor is mor1kx-dev-env.

--- scripts/hardware.sh
+++ scripts/hardware.sh
@@ -5,18 +5,17 @@ SOURCE_CODE_PATH=$DECLAIMER_PATH/sourcecode
 TOOLS_PATH=$DECLAIMER_PATH/tools
 
 # Download source code from github
-echo "Get or1k-xxx from openrisc-declaimer github ..."
+echo "Get mor1kx-dev-env from openrisc-declaimer github ..."
 mkdir -p $SOURCE_CODE_PATH; cd $SOURCE_CODE_PATH
-if [ -d cd or1k-xxx ]; then 
-	cd or1k-xxx; git pull
+if [ -d mor1kx-dev-env ]; then 
+	cd mor1kx-dev-env; git pull
 else
-	git clone https://github.com/openrisc-declaimer/or1k-xxx.git
+	git clone https://github.com/openrisc-declaimer/mor1kx-dev-env.git
 fi
 
-# Build full GCC
-cd $DECLAIMER_PATH; mkdir -p build_gcc; cd build_gcc; rm -rf *
-$SOURCE_CODE_PATH/or1k-gcc/configure --target=or1k-elf \
-                                     --prefix=$TOOLS_PATH/or1kgcc-5.3.0 \
-					                 --enable-languages=c,c++ --disable-shared --disable-libssp --with-newlib
-make; make install
+# Build ...
+mkdir -p $DECLAIMER_PATH/hardware; cd $DECLAIMER_PATH/hardware; rm -rf *
+cp $SOURCE_CODE_PATH/mor1kx-dev-env . -rf
+cd $DECLAIMER_PATH
+
 
